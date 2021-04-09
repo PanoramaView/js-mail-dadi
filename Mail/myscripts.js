2ee18889df216acad3a43563c7678961a0acc4ac
PanoramaView <zhou-1997@hotmail.it>
2021-04-09 16:51:22 +0200

    boh, il push non funziona

--- Mail/myscripts.js
+++ Mail/myscripts.js
@@ -2,13 +2,16 @@ document.write("MAIL");
 console.log("Mail \n");
 
 var a = false
+var x = parseInt(0);
 while(!a){
     
-    var emailList = [];
+    var emailList = new Array(x);
     //console.log("Registered email: "+ emailList(0));
     var newEmail = prompt("Inserire nuova email:");
         emailList.push(newEmail);
-        console.log("New registered email: "+ newEmail);
+        x++;
+        
+        console.log("New registered email: "+ emailList[x]);
         document.write("");
     
         var risposta = false;
@@ -24,9 +27,26 @@ while(!a){
             }
         }
 }
-console.log("\n Number of registered emails: "+ newEmail.length);
+console.log("\n Number of registered emails: "+ emailList.length);
 console.log("\n Registered email list:");
 
 for(var i = 0; i < emailList.length; i++){
     console.log((i+1) +".Registered email: "+ emailList[i]);
 }
+
+var inputEmail= prompt("Inserisci email registrato: ");
+var check = false
+
+for(var i = 0; i < emailList.length; i++){
+    var checkEmail = emailList[i];
+    if(checkEmail === inputEmail){
+        check=true;
+    }
+}
+
+if(check){
+    console.log("Email è presente nel database.")
+} else{
+    console.log("Email non è presente nel database.")
+}
+
